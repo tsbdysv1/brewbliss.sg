7334519e9a86543f2398738f2c4544854bc845fc
tsbdysv1 <tsbdysv1@users.noreply.github.com>
2026-04-15 19:22:41 -0400

    Update pain au chocolat image

--- src/data/menu.js
+++ src/data/menu.js
@@ -3,7 +3,7 @@ import { formatCurrencyVnd } from '../utils/commerce'
 const imageLibrary = {
   plainCroissant: 'https://images.unsplash.com/photo-1509440159596-0249088772ff?auto=format&fit=crop&w=1200&q=80',
   lavaCroissant: 'https://images.unsplash.com/photo-1549903072-7e6e0bedb7fb?auto=format&fit=crop&w=1200&q=80',
-  painAuChocolat: '/images/signature-collection/04.png',
+  painAuChocolat: '/images/signature-collection/uploads-apr15/pain-au-chocolat.png',
   cinnamonRoll: '/images/signature-collection/uploads-apr15/cinnamon-roll.png',
   espresso: 'https://images.unsplash.com/photo-1517701550927-30cf4ba1fcef?auto=format&fit=crop&w=1200&q=80',
   americano: '/images/signature-collection/uploads-apr15/americano.png',
